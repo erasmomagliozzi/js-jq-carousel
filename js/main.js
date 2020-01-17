3ad4fcebc0d69c8695103ae325a3fa3d6a8d35f0
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-17 16:52:04 +0100

    wip

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(
+  function(){
+    $('.next').click(
+      function(){
+        clickNext();
+      });
+
+    $('.prev').click(
+      function(){
+        clickPrev();
+      }
+    )
+  }
+);
+
+
+  function clickNext(){
+
+    var imageActive = $('img.active');
+    var imageNext = imageActive.next();
+    var circleActive = $('i.active');
+    var circleNext = circleActive.next();
+
+    if(imageActive.hasClass('last') == true){
+          imageActive.removeClass('active');
+          $('img.first').addClass('active');
+          circleActive.removeClass('active');
+          $('i.first').addClass('active')
+        }else{
+          imageActive.removeClass('active');
+          imageNext.addClass('active');
+          circleActive.removeClass('active');
+          circleNext.addClass('active');
+        }
+      }
+
+  function clickPrev(){
+
+    var imageActive = $('img.active');
+    var imagePrev = imageActive.prev();
+    var circleActive = $('i.active');
+    var circlePrev = circleActive.prev();
+
+    if(imageActive.hasClass('first') == true){
+          imageActive.removeClass('active');
+          $('img.last').addClass('active');
+          circleActive.removeClass('active');
+          $('i.last').addClass('active');
+        }else{
+          imageActive.removeClass('active');
+          imagePrev.addClass('active');
+          circleActive.removeClass('active');
+          circlePrev.addClass('active');
+        }
+  }
+
+$(document).keydown(
+  function(){
+    if(event.keyCode == 13){
+      clickNext();
+    } else if(event.keyCode == 46){
+      clickPrev();
+    }
+  }
+)
